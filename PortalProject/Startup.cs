4bf15120fa5744ccbfbb4c2c25115451e49e054e
Oguzhan Celik <ogzzcelik@gmail.com>
2021-11-21 23:32:40 +0300

    Layout sayfası düzenlendi.

--- PortalProject/Startup.cs
+++ PortalProject/Startup.cs
@@ -35,7 +35,7 @@ namespace PortalProject
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/PortalkahveCustomer/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace PortalProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=PortalkahveCustomer}/{action=Index}/{id?}");
             });
         }
     }
